3e31105985fecc9d8807dcfac524df8acc23b065
joyhsu <joyhsu0504@gmail.com>
2016-02-06 10:39:33 -0800

    ios project

---
+++ Intrn/Intrn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Intrn
+//
+//  Created by Joy Hsu on 2/6/16.
+//  Copyright © 2016 Joy Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Intrn/Intrn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Intrn
+//
+//  Created by Joy Hsu on 2/6/16.
+//  Copyright © 2016 Joy Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
